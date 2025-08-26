3a10fcc203a486b16dea87d9b5aec16635e57dc1
Sana Khan <SANAKHAN23861@GMAIL.COM>
2025-08-26 21:02:44 +0530

    Create aap.py

---
+++ Frontend/aap.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from add_update_ui import add_update_tab
+from analytics_category import analytics_by_category
+from analytics_months import analytics_by_months
+
+API_URL="http://localhost:8000"
+
+st.title("Expense Management System")
+tab1,tab2,tab3=st.tabs(["Add/Updates","Analytics by Category","Analytics by Months"])
+
+with tab1:
+    add_update_tab()
+
+with tab2:
+    analytics_by_category()
+with tab3:
+    analytics_by_months()
